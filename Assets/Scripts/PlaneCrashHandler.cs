eb1ef4682c7016865e3707edbae2c006b7c29122
UncleYobo <jhsproyer@gmail.com>
2020-05-29 12:39:57 -1000

    plane infrastructure added

---
+++ Assets/Scripts/PlaneCrashHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneCrashHandler : MonoBehaviour
+{
+    public Rigidbody planeRB;
+    public float crashThreshold;
+    public float velocity;
+    public GameObject crashObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        planeRB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        velocity = planeRB.velocity.magnitude;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (planeRB.velocity.magnitude >= crashThreshold) KillPlane();
+    }
+
+    void KillPlane()
+    {
+        Debug.Log("Plane Crashed");
+    }
+}
